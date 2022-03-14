6fbc97c0608163ffc38936fa1b81df7ab7d419ea
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-03-14 17:10:54 -0300

    Player Run, Animation and flip added
    PlayerRun animation created;
    
    Animation swap;
    
    player flip

--- Assets/2.Scripts/Player/PlayerMovement.cs
+++ Assets/2.Scripts/Player/PlayerMovement.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float horizontalSpeed;
+    bool isFacingLeft;
 
     Rigidbody2D rb;
+    Animator  anim;
+    
+    SpriteRenderer sp;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
     }
+    
     void FixedUpdate()
     {
         Move();
@@ -19,5 +26,26 @@ public class PlayerMovement : MonoBehaviour
     void Move()
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal") * Time.deltaTime * horizontalSpeed, rb.velocity.y);
+        anim.SetInteger("movement", (int)Input.GetAxisRaw("Horizontal"));
+
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            if(isFacingLeft)
+                Flip();
+        }
+        else if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            if(!isFacingLeft)
+                Flip();
+        }
+    }
+
+    void Flip()
+    {
+        isFacingLeft = !isFacingLeft;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
