dbfa1092c5928892b437dd6ca6b3fc33f44b8213
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-03-15 17:59:56 -0300

    Player Attack
    Player attack added to player

---
+++ Assets/2.Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using System;
+
+public class PlayerAttack : MonoBehaviour
+{
+    [SerializeField] AnimationClip attackClip;
+   
+    private const float ANIMATION_OFFSET = 0.2f;
+    private Animator anim;
+
+    void Start() => anim = GetComponent<Animator>();
+  
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            StartCoroutine(Attack());      
+    }
+
+    IEnumerator Attack()
+    {
+        anim.SetBool("Attacking", true);
+        yield return new WaitForSeconds(attackClip.length + ANIMATION_OFFSET);
+        anim.SetBool("Attacking", false);
+    }
+
+   
+}
