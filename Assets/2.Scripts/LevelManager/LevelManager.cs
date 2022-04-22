398e542b0b36ec609855012654884a8963d03b33
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-04-22 17:28:03 -0300

    22/04/2022

---
+++ Assets/2.Scripts/LevelManager/LevelManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] private ushort buildIndex; 
+    [SerializeField] private Image screenFade;
+    [SerializeField] private Image screenFill;
+
+    void Awake() 
+    {
+        screenFade.enabled = true;
+        FadeOut();
+    }
+
+    IEnumerator ChangeLevel() 
+    {
+     
+        AsyncOperation async = SceneManager.LoadSceneAsync(buildIndex);
+        async.allowSceneActivation = false;
+
+        DOTweenModuleUI.DOFillAmount(screenFill, 1, .8f);
+        yield return new WaitForSeconds(.81f);
+
+        async.allowSceneActivation = true;
+    }
+
+    void FadeOut() 
+    {
+        DOTweenModuleUI.DOFade(screenFade, 0, 0.7f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(other.gameObject.CompareTag("Player"))
+            StartCoroutine(ChangeLevel());
+        
+    }
+}
