224a8b57aa9b2d719a4a8a469e92daa2c9fa40e8
Wesley Tavares <91393560+WesleyTavaresDev@users.noreply.github.com>
2022-04-22 22:25:48 -0300

    Shop Manager class and IInteract interface created

---
+++ Assets/2.Scripts/Shop/ShopManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
